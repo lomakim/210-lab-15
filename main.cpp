66a15fd13487dcdaf765e7ca099d6e2475206be5
lomakim <lomakim@gmail.com>
2026-03-01 14:16:58 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 // COMSC-210 | Lab 15 | Loma Kim
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Movie{
+    private:
+    string writer;
+    int year;
+    string title;
+
+    public:
+    //MUTATORS
+    void setScreenWriter(string w) { writer = w; }
+    void setReleaseYear(int y) { year = y; }
+    void setMovieTitle(string t) { title = t; }
+
+    //ACCESSORS
+    string getScreenWriter() const { return writer; }
+    int getReleaseYear() const { return year; }
+    string getMovieTitle() const { return title; }
+
+    void printMovie() const;
+};
+
+int main() {
+
+    return 0;
+}
+
+void Movie::printMovie() const {
+    
+}
